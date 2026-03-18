95fe4f219923c2b63fd2c95c8e62fed014f5272d
IARA COHEN LOZIE <50289096@ort>
2026-03-18 09:01:35 -0300

    Primer gitProyect

---
+++ Alumno.cs
@@ -0,0 +1,14 @@
+namespace TP0._5
+
+Public class Alumno{}
+{
+    private int dni;
+    private string nombre;
+    private double faltas;
+
+    public Alum (int dni, string nombre, double faltas)
+    {
+        dni
+    }
+
+}
---
+++ curso.cs
@@ -0,0 +1,17 @@
+namespace TP0._5
+
+public class Curso{
+{
+    private int alum;
+    private int alumDni;
+    private string alumInfo;
+    List<int> listAlum = new List<int>
+
+    private int buscarAlumno(int dni, string nombre, double faltas)
+    {
+        int alumnoBuscadoD = 0;
+        Console.WriteLine("ingrese dni del alumno que quiere buscar");
+        alumnoBuscadoD = int.Parse(Console.ReadLine(alumDni));
+        alumnoBuscadoD = alumDni;
+    }
+}
