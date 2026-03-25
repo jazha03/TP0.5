c9815089a221b2cb053a6cfcb1db41c8b02c14ee
IARA COHEN LOZIE <50289096@ort>
2026-03-25 09:05:14 -0300

    modificacionDic

--- Alumno.cs
+++ Alumno.cs
@@ -2,30 +2,40 @@ namespace TP0._5{
 
 public class Alumno
 {
-    public int dni;
     public string nombre;
     public double faltas;
 
+    Dictionary<int, Alumno> dicAlumno = new Dictionary<int, Alumno>();
+
     public Alumno(int dni, string nombre)
     {
-        this.dni = dni;
         this.nombre = nombre;
         this.faltas = 0;
     }
+
+    foreach(int clave in dicAlumno.Keys)
+    {
+        Console.WriteLine(clave);
+        Console.WriteLine(dicAlumnos[clave]);
+    }
+    
     public void mostrarInfo()
     {
         Console.WriteLine("Nombre: " + nombre);
         Console.WriteLine("DNI: " + dni);
         Console.WriteLine("Faltas: " + faltas);
     }
+
     public void agregarFalta(double cantidad)
     {
         this.faltas += cantidad;
     }
+
     public int getDni()
     {
         return this.dni;
     }
+    
     public double getFaltas()
     {
         return this.faltas;
